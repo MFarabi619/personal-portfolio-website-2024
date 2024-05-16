04afbc4f1a11b191e7d54da0155f347e5fd9b253
Mumtahin Farabi <mfarabi619@gmail.com>
2024-05-16 18:02:02 -0400

    style: Configure Prettier and Eslint, and run both on project

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,13 +1,13 @@
-import {addons} from "@storybook/manager-api"
-import {create} from "@storybook/theming"
+import {addons} from '@storybook/manager-api'
+import {create} from '@storybook/theming'
 
 addons.setConfig({
-// theme: themes.dark,
-theme: create({
-    base: "dark",
+    // theme: themes.dark,
+    theme: create({
+        base: 'dark',
 
-    brandTitle: "Mumtahin Farabi",
-    brandUrl: "https://mfarabi.dev",
-    brandImage: ""
-  })
+        brandTitle: 'Mumtahin Farabi',
+        brandUrl: 'https://mfarabi.dev',
+        brandImage: '',
+    }),
 })
